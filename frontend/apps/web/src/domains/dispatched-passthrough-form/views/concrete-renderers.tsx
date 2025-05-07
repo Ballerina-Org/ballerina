9b99c61e2fbe7049648917c26569b2daace8ec0f
John Fletcher <john.fletcher@blp-digitial.com>
2025-05-07 21:22:42 +0200

    run prettier

--- frontend/apps/web/src/domains/dispatched-passthrough-form/views/concrete-renderers.tsx
+++ frontend/apps/web/src/domains/dispatched-passthrough-form/views/concrete-renderers.tsx
@@ -158,35 +158,35 @@ export const PersonConcreteRenderers = {
       (props) => {
         return (
           <>
-                {props.context.layout.valueSeq().map((tab) =>
-                  tab.columns.valueSeq().map((column) => (
-                    <div style={{ display: "block", float: "left" }}>
-                      {column.groups.valueSeq().map((group) =>
-                        group
-                          .filter((fieldName) =>
-                            props.VisibleFieldKeys.has(fieldName),
-                          )
-                          .map((fieldName) => (
-                            <>
-                              {/* <>{console.debug("fieldName", fieldName)}</> */}
-                              <div style={{ display: "block" }}>
-                                <h3>{fieldName}</h3>
-                                {props.EmbeddedFields.get(fieldName)!({
-                                  ...props,
-                                  context: {
-                                    ...props.context,
-                                    disabled:
-                                      props.DisabledFieldKeys.has(fieldName),
-                                  },
-                                  view: unit,
-                                })}
-                              </div>
-                            </>
-                          )),
-                      )}
-                    </div>
-                  )),
-                )}
+            {props.context.layout.valueSeq().map((tab) =>
+              tab.columns.valueSeq().map((column) => (
+                <div style={{ display: "block", float: "left" }}>
+                  {column.groups.valueSeq().map((group) =>
+                    group
+                      .filter((fieldName) =>
+                        props.VisibleFieldKeys.has(fieldName),
+                      )
+                      .map((fieldName) => (
+                        <>
+                          {/* <>{console.debug("fieldName", fieldName)}</> */}
+                          <div style={{ display: "block" }}>
+                            <h3>{fieldName}</h3>
+                            {props.EmbeddedFields.get(fieldName)!({
+                              ...props,
+                              context: {
+                                ...props.context,
+                                disabled:
+                                  props.DisabledFieldKeys.has(fieldName),
+                              },
+                              view: unit,
+                            })}
+                          </div>
+                        </>
+                      )),
+                  )}
+                </div>
+              )),
+            )}
           </>
         );
       },
--- frontend/libraries/ballerina-core/src/value-infinite-data-stream/state.ts
+++ frontend/libraries/ballerina-core/src/value-infinite-data-stream/state.ts
@@ -278,7 +278,7 @@ export const ValueInfiniteStreamState = {
           ValueInfiniteStreamState.Updaters.Core.chunkStates(
             MapRepo.Updaters.upsert(
               chunkIndex,
-              () => StateChunk.Default({[chunkStateValueKey]: Map()}),
+              () => StateChunk.Default({ [chunkStateValueKey]: Map() }),
               MapRepo.Updaters.update(chunkStateValueKey, stateUpdater),
             ),
           ),
