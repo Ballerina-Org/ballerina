393608a8101aa1d25f9664a214dc622cedc495d2
Davide Walder <davide.walder@gmail.com>
2025-05-15 15:24:49 +0200

    refactor: restructured tests

--- backend/libraries/ballerina-core-python/tests/parsing/primitives_test.py
+++ backend/libraries/ballerina-core-python/tests/parsing/primitives_test.py
@@ -1,10 +1,8 @@
 import math
 from decimal import Decimal
 
-from ballerina_core.option import Option
 from ballerina_core.parsing.dictionary import dict_from_json, dict_to_json
 from ballerina_core.parsing.list import list_from_json, list_to_json
-from ballerina_core.parsing.option import option_from_json, option_to_json
 from ballerina_core.parsing.parsing_types import Json
 from ballerina_core.parsing.primitives import (
     bool_from_json,
@@ -19,8 +17,6 @@ from ballerina_core.parsing.primitives import (
     str_to_json,
 )
 from ballerina_core.parsing.products import tuple1_from_json, tuple1_to_json, tuple2_from_json, tuple2_to_json
-from ballerina_core.parsing.sum import sum_from_json, sum_to_json
-from ballerina_core.sum import Sum
 
 
 class TestPrimitivesSerializer:
@@ -135,63 +131,3 @@ class TestDictionarySerializer:
         deserializer = dict_from_json(str_from_json, int_from_json)
         value = deserializer(serialized)
         assert value == {"a": 42, "b": 43}
-
-
-class TestOptionSerializer:
-    @staticmethod
-    def test_option_to_json_some() -> None:
-        value = Option.some(42)
-        serializer = option_to_json(int_to_json)
-        serialized = serializer(value)
-        assert serialized == {"discriminator": "some", "value": 42}
-
-    @staticmethod
-    def test_option_to_json_none() -> None:
-        value: Option[int] = Option.nothing()
-        serializer = option_to_json(int_to_json)
-        serialized = serializer(value)
-        assert serialized == {"discriminator": "nothing", "value": None}
-
-    @staticmethod
-    def test_option_from_json_some() -> None:
-        serialized: Json = {"discriminator": "some", "value": 42}
-        deserializer = option_from_json(int_from_json)
-        value = deserializer(serialized)
-        assert value == Option.some(42)
-
-    @staticmethod
-    def test_option_from_json_none() -> None:
-        serialized: Json = {"discriminator": "nothing", "value": None}
-        deserializer = option_from_json(int_from_json)
-        value = deserializer(serialized)
-        assert value == Option.nothing()
-
-
-class TestSumSerializer:
-    @staticmethod
-    def test_sum_to_json_left() -> None:
-        value = Sum[int, str].left(42)
-        serializer = sum_to_json(int_to_json, str_to_json)
-        serialized = serializer(value)
-        assert serialized == {"discriminator": "left", "value": 42}
-
-    @staticmethod
-    def test_sum_to_json_right() -> None:
-        value = Sum[int, str].right("42")
-        serializer = sum_to_json(int_to_json, str_to_json)
-        serialized = serializer(value)
-        assert serialized == {"discriminator": "right", "value": "42"}
-
-    @staticmethod
-    def test_sum_from_json_left() -> None:
-        serialized: Json = {"discriminator": "left", "value": 42}
-        deserializer = sum_from_json(int_from_json, str_from_json)
-        value = deserializer(serialized)
-        assert value == Sum[int, str].left(42)
-
-    @staticmethod
-    def test_sum_from_json_right() -> None:
-        serialized: Json = {"discriminator": "right", "value": "42"}
-        deserializer = sum_from_json(int_from_json, str_from_json)
-        value = deserializer(serialized)
-        assert value == Sum[int, str].right("42")
