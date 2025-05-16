97870b8349454370baee8809424d1233a8725712
Davide Walder <davide.walder@gmail.com>
2025-05-16 17:48:44 +0200

    refactor: use same names as in ballerina

--- backend/libraries/ballerina-core-python/ballerina_core/parsing/primitives.py
+++ backend/libraries/ballerina-core-python/ballerina_core/parsing/primitives.py
@@ -5,11 +5,11 @@ from decimal import Decimal
 from ballerina_core.parsing.parsing_types import Json
 
 
-def str_to_json(value: str) -> str:
+def string_to_json(value: str) -> str:
     return value
 
 
-def str_from_json(value: Json) -> str:
+def string_from_json(value: Json) -> str:
     match value:
         case str():
             return value
@@ -29,11 +29,11 @@ def int_from_json(value: Json) -> int:
             raise ValueError(f"Not an int: {value}")
 
 
-def none_to_json() -> Json:
+def unit_to_json() -> Json:
     return None
 
 
-def none_from_json(value: Json) -> None:
+def unit_from_json(value: Json) -> None:
     match value:
         case None:
             return
@@ -53,11 +53,11 @@ def bool_from_json(value: Json) -> bool:
             raise ValueError(f"Not a bool: {value}")
 
 
-def decimal_to_json(value: Decimal) -> Json:
+def float_to_json(value: Decimal) -> Json:
     return str(value)
 
 
-def decimal_from_json(value: Json) -> Decimal:
+def float_from_json(value: Json) -> Decimal:
     match value:
         case str():
             return Decimal(value)
