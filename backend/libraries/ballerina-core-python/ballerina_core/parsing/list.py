f7298a77086460f93419714ffac1467e747a999a
Davide Walder <davide.walder@gmail.com>
2025-05-18 12:15:02 +0200

    fix: renmed variable

--- backend/libraries/ballerina-core-python/ballerina_core/parsing/list.py
+++ backend/libraries/ballerina-core-python/ballerina_core/parsing/list.py
@@ -20,12 +20,12 @@ def list_to_json(item_to_json: ToJson[_A]) -> ToJson[list[_A]]:
 def list_from_json(item_from_json: FromJson[_A]) -> FromJson[list[_A]]:
     def from_json(value: Json) -> list[_A]:
         match value:
-            case {"kind": "list", "elements": tuple_value}:
-                match tuple_value:
+            case {"kind": "list", "elements": elements}:
+                match elements:
                     case list():
-                        return [item_from_json(item) for item in tuple_value]
+                        return [item_from_json(item) for item in elements]
                     case _:
-                        raise ValueError(f"Not a list: {tuple_value}")
+                        raise ValueError(f"Not a list: {elements}")
             case _:
                 raise ValueError(f"Not a list: {value}")
 
