689d5ddb6b5a1e423cfd9495d4d67305cc430f20
Davide Walder <davide.walder@gmail.com>
2025-05-18 11:06:09 +0200

    test: adapted tests

--- backend/libraries/ballerina-core-python/tests/parsing/primitives_test.py
+++ backend/libraries/ballerina-core-python/tests/parsing/primitives_test.py
@@ -75,11 +75,11 @@ class TestListSerializer:
         value = [1, 2, 3]
         serializer = list_to_json(int_to_json)
         serialized = serializer(value)
-        assert serialized == {"kind": "tuple", "elements": [int_to_json(1), int_to_json(2), int_to_json(3)]}
+        assert serialized == {"kind": "list", "elements": [int_to_json(1), int_to_json(2), int_to_json(3)]}
 
     @staticmethod
     def test_list_from_json() -> None:
-        serialized: Json = {"kind": "tuple", "elements": [int_to_json(1), int_to_json(2), int_to_json(3)]}
+        serialized: Json = {"kind": "list", "elements": [int_to_json(1), int_to_json(2), int_to_json(3)]}
         parser = list_from_json(int_from_json)
         value = parser(serialized)
         assert value == [1, 2, 3]
