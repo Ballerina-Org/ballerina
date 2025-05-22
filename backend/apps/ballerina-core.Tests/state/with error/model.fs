50761335b723a97d4855db613cfdc6e5a5f143c6
Davide Walder <davide.walder@gmail.com>
2025-05-22 11:22:38 +0200

    refactor: use updaters

--- backend/apps/ballerina-core.Tests/state/with error/model.fs
+++ backend/apps/ballerina-core.Tests/state/with error/model.fs
@@ -5,25 +5,43 @@ open Ballerina.Collections.Sum
 open Ballerina.Errors
 open NUnit.Framework
 
-type private InnerState = { age: int }
+type private InnerState =
+  { age: int }
 
-type private OuterState = { inner: InnerState; name: string }
+  static member Updaters = {| Age = fun u s -> { s with age = u s.age } |}
+
+type private OuterState =
+  { inner: InnerState
+    name: string }
+
+  static member Getters =
+    {| Inner = fun s -> s.inner
+       Name = fun s -> s.name |}
+
+  static member Replacers =
+    {| Inner = fun v s -> { s with inner = v }
+       Name = fun v s -> { s with name = v } |}
 
 
 [<Test>]
 let ``Should correctly map state`` () =
-  let withInnerState: State<int, unit, InnerState, Errors> =
-    State.State(fun (_, s) -> Sum.Left(1, Some { s with age = s.age + 1 }))
+  let value = "some value"
 
-  let withOuterState: State<int, unit, OuterState, Errors> =
+  let withInnerState: State<string, unit, InnerState, Errors> =
+    state {
+      do! state.SetState(InnerState.Updaters.Age((+) 1))
+      value
+    }
+
+  let withOuterState: State<string, unit, OuterState, Errors> =
     withInnerState
-    |> State.mapState (fun output -> output.inner) (fun inner outer -> { outer with inner = inner })
+    |> State.mapState OuterState.Getters.Inner OuterState.Replacers.Inner
 
   let result = withOuterState.run ((), { inner = { age = 30 }; name = "John" })
 
   match result with
   | Sum.Left(res, state) ->
-    Assert.That(res, Is.EqualTo 1)
+    Assert.That(res, Is.EqualTo value)
 
     match state with
     | Some s -> Assert.That(s, Is.EqualTo { inner = { age = 31 }; name = "John" })
