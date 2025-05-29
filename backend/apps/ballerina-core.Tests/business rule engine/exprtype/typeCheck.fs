2d757b7b87ef8bf973846f972f919aee6cd651b5
Davide Walder <davide.walder@gmail.com>
2025-05-29 10:32:00 +0200

    test: more tests

--- backend/apps/ballerina-core.Tests/business rule engine/exprtype/typeCheck.fs
+++ backend/apps/ballerina-core.Tests/business rule engine/exprtype/typeCheck.fs
@@ -6,10 +6,18 @@ open Ballerina.DSL.Expr.Types.Model
 open Ballerina.DSL.Expr.Model
 open Ballerina.DSL.Expr.Types.TypeCheck
 open Ballerina.DSL.Model
+open Ballerina.Errors
 
-type TestCase = { expr: Expr; expected: ExprType }
+let private typeCheck (expr: Expr) : Sum<ExprType * VarTypes, Errors> =
+  let schema: Schema =
+    { tryFindEntity = fun _ -> None
+      tryFindField = fun _ -> None }
 
-let testCases =
+  Expr.typeCheck Map.empty schema Map.empty expr
+
+type ValuePrimitiveTypeCheckTestCase = { expr: Expr; expected: ExprType }
+
+let valuePrimitiveTypeCheckTestCases =
   [ { expr = Expr.Value(Value.ConstInt 42)
       expected = ExprType.PrimitiveType PrimitiveType.IntType }
     { expr = Expr.Value(Value.ConstString "42")
@@ -17,18 +25,28 @@ let testCases =
     { expr = Expr.Value(Value.ConstBool true)
       expected = ExprType.PrimitiveType PrimitiveType.BoolType } ]
 
-[<TestCaseSource(nameof testCases)>]
-let ``Should typecheck values primitives`` (testCase: TestCase) =
+[<TestCaseSource(nameof valuePrimitiveTypeCheckTestCases)>]
+let ``Should typecheck values primitives`` (testCase: ValuePrimitiveTypeCheckTestCase) =
   let { expr = expr; expected = expected } = testCase
 
-  let schema: Schema =
-    { tryFindEntity = fun _ -> None
-      tryFindField = fun _ -> None }
-
-  let inferredType = Expr.typeCheck Map.empty schema Map.empty expr
-
-  match inferredType with
+  match typeCheck expr with
   | Left(value, varTypes) ->
     Assert.That(value, Is.EqualTo expected)
     Assert.That(varTypes, Is.Empty)
-  | Right err -> Assert.Fail($"Expected success but got error: {err}")
+  | Right err -> Assert.Fail $"Expected success but got error: {err}"
+
+type BoolReturningBinaryExpressionTestCase = { expr: Expr; expected: ExprType }
+
+let boolReturningBinaryExpressionTestCases =
+  [ { expr = Expr.Binary(Or, Expr.Value(Value.ConstBool true), Expr.Value(Value.ConstBool false))
+      expected = ExprType.PrimitiveType PrimitiveType.BoolType } ]
+
+[<TestCaseSource(nameof boolReturningBinaryExpressionTestCases)>]
+let ``Should typecheck bool returning binary expressions`` (testCase: BoolReturningBinaryExpressionTestCase) =
+  let { expr = expr; expected = expected } = testCase
+
+  match typeCheck expr with
+  | Left(value, varTypes) ->
+    Assert.That(value, Is.EqualTo expected)
+    Assert.That(varTypes, Is.Empty)
+  | Right err -> Assert.Fail $"Expected success but got error: {err}"
