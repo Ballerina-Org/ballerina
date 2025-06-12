bcafa6111099db6df030822379dd643dd32088c8
PawelStadnicki <pawel.stadnicki@gmail.com>
2025-06-12 22:50:37 +0200

    refresh from main

--- backend/apps/ide-api/Spec.fs
+++ backend/apps/ide-api/Spec.fs
@@ -1,12 +1,4 @@
 ﻿namespace IDEApi
 
-open System
-
 type Spec =
     { Name: string }
-
-    member this.length =
-        this.Name.Length
-                
-// type SpecRequest = { specBody: string }
-// type SpecValidationResult<'T> = { isValid: bool } //; Payload: 'T }
