5234dc0ad1c4fa816b13794add3c8d61574bebf5
Stefano Andriolo <stefano.andriolo@blp-digital.com>
2025-06-24 09:39:08 +0200

    fix: remove unnecessary check

--- frontend/libraries/ballerina-core/src/forms/domains/dispatched-forms/runner/domains/dispatcher/domains/table/state.ts
+++ frontend/libraries/ballerina-core/src/forms/domains/dispatched-forms/runner/domains/dispatcher/domains/table/state.ts
@@ -153,11 +153,12 @@ export const TableDispatcher = {
                                   )
                                     .mapContext((_: any) => ({
                                       ..._,
-                                      apiMethods: !api || typeof api !== "string"
-                                        ? []
-                                        : (dispatcherContext.specApis.tables?.get(
-                                            api!,
-                                          )?.methods ?? []),
+                                      apiMethods:
+                                        typeof api !== "string"
+                                          ? []
+                                          : (dispatcherContext.specApis.tables?.get(
+                                              api!,
+                                            )?.methods ?? []),
                                       ...(!isNested && launcherName
                                         ? {
                                             identifiers: {
