65309cfc5ace7045585ce8b5228c7dd545dd016b
Nikita Tchayka <github@nickseagull.dev>
2025-07-15 17:38:27 +0100

    feat(readOnly): add DefaultReadOnly constructor

--- backend/libraries/ballerina-core-go/readOnly.go
+++ backend/libraries/ballerina-core-go/readOnly.go
@@ -8,6 +8,10 @@ type DeltaReadOnly[a any, deltaA any] struct {
 	DeltaBase
 }
 
+func DefaultReadOnly[T any]() ReadOnly[T] {
+	return ReadOnly[T]{}
+}
+
 func MatchDeltaReadOnly[a any, deltaA any, Result any]() func(DeltaReadOnly[a, deltaA]) (Result, error) {
 	return func(delta DeltaReadOnly[a, deltaA]) (Result, error) {
 		var result Result
