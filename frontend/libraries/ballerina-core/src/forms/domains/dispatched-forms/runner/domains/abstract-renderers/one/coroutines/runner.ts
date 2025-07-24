d9636ce1a241529fbcc75af9618503563f35253e
Stefano Andriolo <stefano.andriolo@blp-digital.com>
2025-07-24 15:59:59 +0200

    feat: split coroutines, init one stream

--- frontend/libraries/ballerina-core/src/forms/domains/dispatched-forms/runner/domains/abstract-renderers/one/coroutines/runner.ts
+++ frontend/libraries/ballerina-core/src/forms/domains/dispatched-forms/runner/domains/abstract-renderers/one/coroutines/runner.ts
@@ -2,230 +2,23 @@ import { ValueInfiniteStreamLoader } from "../../../../../../../../value-infinit
 import { ValueInfiniteStreamState } from "../../../../../../../../value-infinite-data-stream/state";
 import {
   OneAbstractRendererForeignMutationsExpected,
-  OneAbstractRendererReadonlyContext,
   OneAbstractRendererState,
 } from "../state";
 import {
-  Debounce,
   SimpleCallback,
   Value,
-  CoTypedFactory,
-  ValueOption,
   Debounced,
-  AsyncState,
-  Synchronize,
-  PredicateValue,
   Unit,
-  Synchronized,
   ValueOrErrors,
   ValueRecord,
-  ValueUnit,
-  DispatchOnChange,
-  replaceWith,
-  id as idUpdater,
-  DispatchDelta,
   BaseFlags,
-  Option,
+  Sum,
 } from "../../../../../../../../../main";
 import { Map } from "immutable";
-
-const Co = <CustomPresentationContext = Unit, ExtraContext = Unit>() =>
-  CoTypedFactory<
-    OneAbstractRendererReadonlyContext<CustomPresentationContext, ExtraContext>,
-    OneAbstractRendererState
-  >();
-const InitializeCo = <
-  CustomPresentationContext = Unit,
-  ExtraContext = Unit,
->() =>
-  CoTypedFactory<
-    OneAbstractRendererReadonlyContext<
-      CustomPresentationContext,
-      ExtraContext
-    > &
-      Pick<OneAbstractRendererForeignMutationsExpected, "onChange">,
-    OneAbstractRendererState
-  >();
-
-const DebouncerCo = <CustomPresentationContext = Unit, ExtraContext = Unit>() =>
-  CoTypedFactory<
-    OneAbstractRendererReadonlyContext<
-      CustomPresentationContext,
-      ExtraContext
-    > & {
-      onDebounce: SimpleCallback<void>;
-    },
-    OneAbstractRendererState
-  >();
-
-const DebouncedCo = CoTypedFactory<
-  { onDebounce: SimpleCallback<void> },
-  Value<Map<string, string>>
->();
-
-const intializeOne = <
-  CustomPresentationContext = Unit,
-  ExtraContext = Unit,
->() =>
-  InitializeCo<CustomPresentationContext, ExtraContext>()
-    .GetState()
-    .then((current) => {
-      const InstantiatedInitializeCo = InitializeCo<
-        CustomPresentationContext,
-        ExtraContext
-      >();
-      if (current.value == undefined) {
-        return InstantiatedInitializeCo.Wait(0);
-      }
-
-      /// When initailising, in both stages, inject the id to the get chunk
-
-      const local = current.bindings.get("local");
-      if (local == undefined) {
-        console.error(
-          `local binding is undefined when intialising one\n... in couroutine for\n...${current.domNodeAncestorPath + "[one]"}`,
-        );
-        return InstantiatedInitializeCo.Wait(0);
-      }
-
-      if (!PredicateValue.Operations.IsRecord(local)) {
-        console.error(
-          `local binding is not a record when intialising one\n... in couroutine for\n...${current.domNodeAncestorPath + "[one]"}`,
-        );
-        return InstantiatedInitializeCo.Wait(0);
-      }
-
-      if (!local.fields.has("Id")) {
-        console.error(
-          `local binding is missing Id (check casing) when intialising one\n... in couroutine for\n...${current.domNodeAncestorPath + "[one]"}`,
-        );
-        return InstantiatedInitializeCo.Wait(0);
-      }
-
-      const id = local.fields.get("Id")!; // safe because of above check;
-      if (!PredicateValue.Operations.IsString(id)) {
-        console.error(
-          `local Id is not a string when intialising one\n... in couroutine for\n...${current.domNodeAncestorPath + "[one]"}`,
-        );
-        return InstantiatedInitializeCo.Wait(0);
-      }
-
-      const initializationCompletedCo = InitializeCo<
-        CustomPresentationContext,
-        ExtraContext
-      >().Seq([
-        InstantiatedInitializeCo.Do(() => {
-          console.debug("initializationCompletedCo");
-        }),
-        InstantiatedInitializeCo.SetState(
-          OneAbstractRendererState.Updaters.Core.customFormState.children
-            .previousRemoteEntityVersionIdentifier(
-              replaceWith(current.remoteEntityVersionIdentifier),
-            )
-            .then(
-              OneAbstractRendererState.Updaters.Core.customFormState.children.shouldReinitialize(
-                replaceWith(false),
-              ),
-            )
-            .then(
-              current.customFormState.status == "open"
-                ? OneAbstractRendererState.Updaters.Core.customFormState.children.stream(
-                    ValueInfiniteStreamState.Updaters.Template.loadMore(),
-                  )
-                : idUpdater,
-            ),
-        ),
-      ]);
-
-      const hasInitialValue =
-        (PredicateValue.Operations.IsOption(current.value) &&
-          current.value.isSome) ||
-        PredicateValue.Operations.IsUnit(current.value);
-
-      const initializeStreamCo = InitializeCo<
-        CustomPresentationContext,
-        ExtraContext
-      >().SetState(
-        OneAbstractRendererState.Updaters.Core.customFormState.children.stream(
-          replaceWith(
-            ValueInfiniteStreamState.Default(
-              100,
-              current.customFormState.getChunkWithParams(id)(
-                current.customFormState.streamParams.value,
-              ),
-            ),
-          ),
-        ),
-      );
-
-      if (hasInitialValue) {
-        return InstantiatedInitializeCo.Seq([
-          initializeStreamCo,
-          initializationCompletedCo,
-        ]);
-      }
-
-      const initializeValueCo = InitializeCo<
-        CustomPresentationContext,
-        ExtraContext
-      >()
-        .Await(
-          () => current.getApi(id),
-          (_) => console.error("err"),
-        )
-        .then((value) =>
-          InstantiatedInitializeCo.Do(() => {
-            return current.fromApiParser(value.value).Then((result) => {
-              const updater = replaceWith<ValueOption | ValueUnit>(
-                ValueOption.Default.some(result),
-              );
-              const delta: DispatchDelta<BaseFlags> = {
-                kind: "OneReplace",
-                replace: result,
-                flags: {
-                  kind: "localOnly",
-                },
-                type: current.type,
-                sourceAncestorLookupTypeNames: current.lookupTypeAncestorNames,
-              };
-              console.debug("initializeOne delta", delta);
-              current.onChange(Option.Default.some(updater), delta);
-
-              return ValueOrErrors.Default.return(result);
-            });
-          }),
-        );
-
-      return InstantiatedInitializeCo.Seq([
-        initializeStreamCo,
-        initializeValueCo,
-        initializationCompletedCo,
-      ]);
-    });
-
-const debouncer = <CustomPresentationContext = Unit, ExtraContext = Unit>() =>
-  DebouncerCo<CustomPresentationContext, ExtraContext>().Repeat(
-    DebouncerCo<CustomPresentationContext, ExtraContext>().Seq([
-      Debounce<
-        Value<Map<string, string>>,
-        { onDebounce: SimpleCallback<void> }
-      >(
-        DebouncedCo.GetState()
-          .then((current) => DebouncedCo.Do(() => current.onDebounce()))
-          //.SetState(SearchNow.Updaters.reloadsRequested(_ => _ + 1))
-          .then((_) => DebouncedCo.Return("success")),
-        250,
-      ).embed(
-        (_) => ({
-          ..._.customFormState.streamParams,
-          onDebounce: _.onDebounce,
-        }),
-        OneAbstractRendererState.Updaters.Core.customFormState.children
-          .streamParams,
-      ),
-      DebouncerCo<CustomPresentationContext, ExtraContext>().Wait(0),
-    ]),
-  );
+import { Co, DebouncerCo, InitializeCo } from "./builder";
+import { initializeOne } from "./_initializeOne";
+import { initializeStream } from "./_initializeStream";
+import { debouncer } from "./_debouncer";
 
 export const initializeOneRunner = <
   CustomPresentationContext = Unit,
@@ -234,13 +27,29 @@ export const initializeOneRunner = <
 >() =>
   InitializeCo<CustomPresentationContext, ExtraContext>().Template<
     OneAbstractRendererForeignMutationsExpected<Flags>
-  >(intializeOne<CustomPresentationContext, ExtraContext>(), {
+  >(initializeOne<CustomPresentationContext, ExtraContext>(), {
     interval: 15,
     runFilter: (props) =>
       // if the value is some, we already have something to pass to the renderers
       // -> we don't have to run the initialization coroutine
       // if the inner value is unit, we are rendering a partial one
       (props.context.value.kind === "option" && !props.context.value.isSome) ||
+      (props.context.customFormState.shouldReinitialize &&
+        props.context.remoteEntityVersionIdentifier !==
+          props.context.customFormState.previousRemoteEntityVersionIdentifier),
+  });
+
+export const initializeStreamRunner = <
+  CustomPresentationContext = Unit,
+  Flags = BaseFlags,
+  ExtraContext = Unit,
+>() =>
+  Co<CustomPresentationContext, ExtraContext>().Template<
+    OneAbstractRendererForeignMutationsExpected<Flags>
+  >(initializeStream<CustomPresentationContext, ExtraContext>(), {
+    interval: 15,
+    runFilter: (props) =>
+      props.context.customFormState.stream.kind === "r" ||
       (props.context.customFormState.shouldReinitialize &&
         props.context.remoteEntityVersionIdentifier !==
           props.context.customFormState.previousRemoteEntityVersionIdentifier),
@@ -270,14 +79,21 @@ export const oneTableLoaderRunner = <
     OneAbstractRendererForeignMutationsExpected<Flags>
   >(
     ValueInfiniteStreamLoader().embed(
-      (_) => _.customFormState.stream,
-      OneAbstractRendererState.Updaters.Core.customFormState.children.stream,
+      (_) =>
+        _.customFormState.stream.kind === "l"
+          ? _.customFormState.stream.value
+          : undefined,
+      (upd) =>
+        OneAbstractRendererState.Updaters.Core.customFormState.children.stream(
+          Sum.Updaters.left(upd),
+        ),
     ),
     {
       interval: 15,
       runFilter: (props) =>
+        props.context.customFormState.stream.kind === "l" &&
         ValueInfiniteStreamState.Operations.shouldCoroutineRun(
-          props.context.customFormState.stream,
+          props.context.customFormState.stream.value,
         ),
     },
   );
