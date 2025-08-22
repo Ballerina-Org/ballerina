3ba4c2f7e6caf79d9316816087813df52d148b11
Joseph Thommes <joseph.thommes@blp-digital.com>
2025-08-22 12:01:46 +0200

    feat: add tests for unit and sum

---
+++ backend/libraries/ballerina-core-go/ballerina_test/serializer_test.go
@@ -0,0 +1,51 @@
+package ballerina_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	ballerina "ballerina.com/core"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUnitSerialization(t *testing.T) {
+	t.Parallel()
+	serializer := ballerina.UnitSerializer()
+	unit := ballerina.Unit{}
+	serialized := serializer(unit)
+	require.Equal(t, ballerina.Right[error, json.RawMessage](json.RawMessage(`{"kind":"unit"}`)), serialized)
+}
+
+func TestUnitDeserialization(t *testing.T) {
+	t.Parallel()
+	deserializer := ballerina.UnitDeserializer()
+	serialized := json.RawMessage(`{"kind":"unit"}`)
+	deserialized := deserializer(serialized)
+	require.Equal(t, ballerina.Right[error, ballerina.Unit](ballerina.Unit{}), deserialized)
+}
+
+func TestSumSerialization(t *testing.T) {
+	t.Parallel()
+	serializer := ballerina.SumSerializer(ballerina.UnitSerializer(), ballerina.UnitSerializer())
+
+	sumLeft := ballerina.Left[ballerina.Unit, ballerina.Unit](ballerina.Unit{})
+	serializedLeft := serializer(sumLeft)
+	require.Equal(t, ballerina.Right[error, json.RawMessage](json.RawMessage(`{"case":"Sum.Left","value":{"kind":"unit"}}`)), serializedLeft)
+
+	sumRight := ballerina.Right[ballerina.Unit, ballerina.Unit](ballerina.Unit{})
+	serializedRight := serializer(sumRight)
+	require.Equal(t, ballerina.Right[error, json.RawMessage](json.RawMessage(`{"case":"Sum.Right","value":{"kind":"unit"}}`)), serializedRight)
+}
+
+func TestSumDeserialization(t *testing.T) {
+	t.Parallel()
+	deserializer := ballerina.SumDeserializer(ballerina.UnitDeserializer(), ballerina.UnitDeserializer())
+
+	serializedLeft := json.RawMessage(`{"case":"Sum.Left","value":{"kind":"unit"}}`)
+	deserializedLeft := deserializer(serializedLeft)
+	require.Equal(t, ballerina.Right[error, ballerina.Sum[ballerina.Unit, ballerina.Unit]](ballerina.Left[ballerina.Unit, ballerina.Unit](ballerina.Unit{})), deserializedLeft)
+
+	serializedRight := json.RawMessage(`{"case":"Sum.Right","value":{"kind":"unit"}}`)
+	deserializedRight := deserializer(serializedRight)
+	require.Equal(t, ballerina.Right[error, ballerina.Sum[ballerina.Unit, ballerina.Unit]](ballerina.Right[ballerina.Unit, ballerina.Unit](ballerina.Unit{})), deserializedRight)
+}
