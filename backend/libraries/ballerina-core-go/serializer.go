5ca4654a27e0b59dcc00b33ff81ceaaea4820466
Joseph Thommes <joseph.thommes@blp-digital.com>
2025-08-26 12:32:20 +0200

    docs: improve explanatory docstring

--- backend/libraries/ballerina-core-go/serializer.go
+++ backend/libraries/ballerina-core-go/serializer.go
@@ -11,7 +11,8 @@ import (
 // The Serializer function should be total (i.e. never return an error).
 // However, we use the json.Marshal to serialize the value under the hood (mainly because of the string serialization in json),
 // which can return an error. In theory, we could wrap json.Marshal and panic on error. For that to be safe,
-// we would have to prove that it can never happen (which we believe is true, but did not formally prove).
+// we would have to prove that we never encounter an error when we serialize. Since we did not prove this for
+// the current implementation -- and it is hard to enforce this invariant if we extend the serialization.
 // Thus, this partial signature.
 
 type Serializer[T any] func(T) Sum[error, json.RawMessage]
