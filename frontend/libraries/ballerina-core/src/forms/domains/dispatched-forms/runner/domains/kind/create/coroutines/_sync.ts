8206f4fc63e6a6ab4f6dbb05a3d035ba91075b73
Stefano Andriolo <dev@srand.it>
2025-09-30 12:30:28 +0200

    fix: remove Co.Repeat

--- frontend/libraries/ballerina-core/src/forms/domains/dispatched-forms/runner/domains/kind/create/coroutines/_sync.ts
+++ frontend/libraries/ballerina-core/src/forms/domains/dispatched-forms/runner/domains/kind/create/coroutines/_sync.ts
@@ -1,7 +1,7 @@
 import {
   ApiErrors,
   ApiResponseChecker,
-  Debounce,
+  AsyncState,
   HandleApiResponse,
   id,
   Synchronize,
@@ -37,69 +37,67 @@ export const syncCo = <
       ),
     );
 
-  return Co.Repeat(
-    Co.GetState().then((current) => {
-      if (current.deserializedSpecification.sync.kind !== "loaded") {
-        return Co.SetState(id);
-      }
+  return Co.GetState().then((current) => {
+    if (current.deserializedSpecification.sync.kind !== "loaded") {
+      return Co.SetState(id);
+    }
 
-      if (current.deserializedSpecification.sync.value.kind == "errors") {
-        return Co.SetState(id);
-      }
+    if (current.deserializedSpecification.sync.value.kind == "errors") {
+      return Co.SetState(id);
+    }
 
-      const createFormLauncher =
-        current.deserializedSpecification.sync.value.value.launchers.create.get(
-          current.launcherRef.name,
-        );
-
-      if (!createFormLauncher) {
-        return Co.SetState(id);
-      }
-
-      const create = current.launcherRef.apiSources.entityApis.create(
-        createFormLauncher.api,
+    const createFormLauncher =
+      current.deserializedSpecification.sync.value.value.launchers.create.get(
+        current.launcherRef.name,
       );
 
-      return Co.Seq([
-        setChecked(false),
-        Synchronize<Unit, ApiErrors, Unit>(
-          (_) => {
-            if (current.entity.sync.kind !== "loaded") {
-              return Promise.resolve([]);
-            }
+    if (!createFormLauncher) {
+      return Co.SetState(id);
+    }
 
-            const parsed = createFormLauncher?.toApiParser(
-              current.entity.sync.value,
-              createFormLauncher?.type,
-              current.formState,
-            );
+    const create = current.launcherRef.apiSources.entityApis.create(
+      createFormLauncher.api,
+    );
 
-            return !parsed || parsed?.kind == "errors"
-              ? Promise.reject(parsed?.errors)
-              : create(parsed.value).then(() => []);
-          },
-          (_) => "transient failure",
-          3,
-          50,
-        ).embed(
-          (_) => _.apiRunner,
-          DispatchCreateFormLauncherState<T, Flags>().Updaters.Core.apiRunner,
-        ),
-        HandleApiResponse<
-          DispatchCreateFormLauncherState<T, Flags>,
-          DispatchCreateFormLauncherContext<
-            T,
-            Flags,
-            CustomPresentationContexts,
-            ExtraContext
-          >,
-          ApiErrors
-        >((_) => _.apiRunner.sync, {
-          handleSuccess: current.launcherRef.apiHandlers?.onCreateSuccess,
-          handleError: current.launcherRef.apiHandlers?.onCreateError,
-        }),
-        setChecked(true),
-      ]);
-    }),
-  );
+    return Co.Seq([
+      setChecked(false),
+      Synchronize<Unit, ApiErrors, Unit>(
+        (_) => {
+          if (current.entity.sync.kind !== "loaded") {
+            return Promise.resolve([]);
+          }
+
+          const parsed = createFormLauncher?.toApiParser(
+            current.entity.sync.value,
+            createFormLauncher?.type,
+            current.formState,
+          );
+
+          return !parsed || parsed?.kind == "errors"
+            ? Promise.reject(parsed?.errors)
+            : create(parsed.value).then(() => []);
+        },
+        (_) => "transient failure",
+        3,
+        50,
+      ).embed(
+        (_) => _.apiRunner,
+        DispatchCreateFormLauncherState<T, Flags>().Updaters.Core.apiRunner,
+      ),
+      HandleApiResponse<
+        DispatchCreateFormLauncherState<T, Flags>,
+        DispatchCreateFormLauncherContext<
+          T,
+          Flags,
+          CustomPresentationContexts,
+          ExtraContext
+        >,
+        ApiErrors
+      >((_) => _.apiRunner.sync, {
+        handleSuccess: current.launcherRef.apiHandlers?.onCreateSuccess,
+        handleError: current.launcherRef.apiHandlers?.onCreateError,
+      }),
+      setChecked(true),
+    ]);
+  });
 };
