0568d6d6ee20787544f70a7235bf4eafdac56970
RonaldGalea <raulronaldgalea@gmail.com>
2025-10-15 18:05:34 +0200

    feat: bring option parser up to speed

--- backend/libraries/ballerina-core-python/tests/parsing/option_test.py
+++ backend/libraries/ballerina-core-python/tests/parsing/option_test.py
@@ -10,25 +10,25 @@ def test_option_to_json_some() -> None:
     value = Option.some(42)
     serializer = option_to_json(int32_to_json, unit_to_json)
     serialized = serializer(value)
-    assert serialized == {"case": "some", "value": int32_to_json(42)}
+    assert serialized == {"discriminator": "union", "value": ["some", int32_to_json(42)]}
 
 
 def test_option_to_json_none() -> None:
     value: Option[int] = Option.none()
     serializer = option_to_json(int32_to_json, unit_to_json)
     serialized = serializer(value)
-    assert serialized == {"case": "none", "value": unit_to_json(unit)}
+    assert serialized == {"discriminator": "union", "value": ["none", unit_to_json(unit)]}
 
 
 def test_option_from_json_some() -> None:
-    serialized: Json = {"case": "some", "value": int32_to_json(42)}
+    serialized: Json = {"discriminator": "union", "value": ["some", int32_to_json(42)]}
     parser = option_from_json(int32_from_json, unit_from_json)
     value = parser(serialized)
     assert value == Sum.right(Option.some(42))
 
 
 def test_option_from_json_none() -> None:
-    serialized: Json = {"case": "none", "value": unit_to_json(unit)}
+    serialized: Json = {"discriminator": "union", "value": ["none", unit_to_json(unit)]}
     parser = option_from_json(int32_from_json, unit_from_json)
     value = parser(serialized)
     assert value == Sum.right(Option.none())
