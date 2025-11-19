9fc3a9b7bbe98b4c1816961ec21b4356e8a80bf1
John Fletcher <john.fletcher@blp-digital.com>
2025-11-19 15:04:33 +0100

    remove field disabled and visible predicates

--- frontend/libraries/ballerina-core/src/forms/domains/dispatched-forms/deserializer/domains/specification/domains/forms/domains/renderer/domains/record/domains/recordFieldRenderer/state.ts
+++ frontend/libraries/ballerina-core/src/forms/domains/dispatched-forms/deserializer/domains/specification/domains/forms/domains/renderer/domains/record/domains/recordFieldRenderer/state.ts
@@ -14,46 +14,20 @@ import {
   SerializedNestedRenderer,
 } from "../../../nestedRenderer/state";
 
-export type SerializedRecordFieldRenderer = {
-  visible?: unknown;
-  disabled?: unknown;
-} & SerializedNestedRenderer;
+export type SerializedRecordFieldRenderer = SerializedNestedRenderer;
 
-export type RecordFieldRenderer<T> = {
-  visible?: Expr;
-  disabled?: Expr;
-} & NestedRenderer<T>;
+export type RecordFieldRenderer<T> = NestedRenderer<T>;
 
 export const RecordFieldRenderer = {
-  ComputeVisibility: (
-    visible: unknown,
-  ): ValueOrErrors<Expr | undefined, string> =>
-    visible == undefined
-      ? ValueOrErrors.Default.return(undefined)
-      : Expr.Operations.parseAsVisibilityExpression(visible),
-  ComputeDisabled: (
-    disabled: unknown,
-  ): ValueOrErrors<Expr | undefined, string> =>
-    disabled == undefined
-      ? ValueOrErrors.Default.return(undefined)
-      : Expr.Operations.parseAsDisabledExpression(disabled),
   tryAsValidRecordFieldRenderer: (
     serialized: unknown,
   ): ValueOrErrors<SerializedRecordFieldRenderer, string> =>
     NestedRenderer.Operations.tryAsValidSerializedNestedRenderer(
       serialized,
     ).Then((deserializedRenderer) =>
-      ValueOrErrors.Default.return<SerializedRecordFieldRenderer, string>({
-        ...deserializedRenderer,
-        visible:
-          isObject(serialized) && "visible" in serialized
-            ? serialized.visible
-            : undefined,
-        disabled:
-          isObject(serialized) && "disabled" in serialized
-            ? serialized.disabled
-            : undefined,
-      }),
+      ValueOrErrors.Default.return<SerializedRecordFieldRenderer, string>(
+        deserializedRenderer,
+      ),
     ),
   Deserialize: <
     T extends DispatchInjectablesTypes<T>,
@@ -78,8 +52,8 @@ export const RecordFieldRenderer = {
     string
   > =>
     RecordFieldRenderer.tryAsValidRecordFieldRenderer(serialized).Then(
-      (validatedSerialized) => {
-        return NestedRenderer.Operations.DeserializeAs(
+      (validatedSerialized) =>
+        NestedRenderer.Operations.DeserializeAs(
           type,
           validatedSerialized,
           concreteRenderers,
@@ -88,23 +62,10 @@ export const RecordFieldRenderer = {
           forms,
           alreadyParsedForms,
         ).Then(([deserializedNestedRenderer, newAlreadyParsedForms]) =>
-          RecordFieldRenderer.ComputeVisibility(
-            validatedSerialized.visible,
-          ).Then((visible) =>
-            RecordFieldRenderer.ComputeDisabled(
-              validatedSerialized.disabled,
-            ).Then((disabled) =>
-              ValueOrErrors.Default.return([
-                {
-                  ...deserializedNestedRenderer,
-                  visible,
-                  disabled,
-                },
-                newAlreadyParsedForms,
-              ]),
-            ),
-          ),
-        );
-      },
+          ValueOrErrors.Default.return([
+            deserializedNestedRenderer,
+            newAlreadyParsedForms,
+          ]),
+        ),
     ),
 };
