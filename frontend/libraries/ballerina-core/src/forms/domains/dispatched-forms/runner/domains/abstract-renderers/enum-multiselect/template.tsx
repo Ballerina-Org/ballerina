cb7e1d57f35aafedaba0eab1c76f647aa9fe25dc
Stefano Andriolo <stefano.andriolo@blp-digital.com>
2025-12-05 14:57:39 +0100

    fix: use dom node path

--- frontend/libraries/ballerina-core/src/forms/domains/dispatched-forms/runner/domains/abstract-renderers/enum-multiselect/template.tsx
+++ frontend/libraries/ballerina-core/src/forms/domains/dispatched-forms/runner/domains/abstract-renderers/enum-multiselect/template.tsx
@@ -56,7 +56,7 @@ export const EnumMultiselectAbstractRenderer = <
       ExtraContext
     >
   >((props) => {
-    const domNodeId = props.context.predictionAncestorPath;
+    const domNodeId = props.context.domNodeAncestorPath;
 
     if (!PredicateValue.Operations.IsRecord(props.context.value)) {
       console.error(
