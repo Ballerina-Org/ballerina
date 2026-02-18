1d795fe509827a417bed3b8b108a8783d4a6ffcc
SuzanAbouKamar <70631444+SuzanAbouKamar@users.noreply.github.com>
2026-02-18 11:45:58 +0100

    Dockerize playground web (#702)
    * Change @rspack/cli version back to 0.5.5 to fix build errors
    
    * Change volume and add user to fix errors when running postgresserver
    
    * Prevent "yarn tsc" from removing watch output to be able to see all (potential) build errors
    
    * Dockerize playground web
    
    * Use user and group ids to prevent access denied errors in node modules when running yarn install

--- frontend/core-startup.sh
+++ frontend/core-startup.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-docker compose run --rm  --build --remove-orphans  ballerina-core-frontend-dependency-installer &&
-docker compose run --rm  --build --remove-orphans  ballerina-core-tsc
+docker compose build --no-cache ballerina-core-frontend-dependency-installer --build-arg UID=$(id -u) --build-arg GID=$(id -g)  && 
+ docker compose run --rm --remove-orphans ballerina-core-frontend-dependency-installer &&
+ docker compose build --no-cache ballerina-core-tsc --build-arg UID=$(id -u) --build-arg GID=$(id -g)  && 
+ docker compose run --rm --remove-orphans ballerina-core-tsc
