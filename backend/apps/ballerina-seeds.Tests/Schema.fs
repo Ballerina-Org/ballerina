ed4002d513bde125e8f9cdeef1ab87864630487c
Paweł Stadnicki <56049414+PawelStadnicki@users.noreply.github.com>
2026-03-11 13:54:56 +0100

    clean zombie deps (#730)

--- backend/apps/ballerina-seeds.Tests/Schema.fs
+++
@@ -1,7 +0,0 @@
-module Ballerina.Data.Tests.Seeds.Schema
-
-open Ballerina
-open Ballerina.Collections.Sum
-open NUnit.Framework
-
-//TODO: prev test are deleted, the new will come with v2 schema extension for guiding seeding
